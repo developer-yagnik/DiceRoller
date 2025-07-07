09e0204b6618e34726116e8eef867230fbaf7f07
developer-yagnik <yagnikpatel5253@gmail.com>
2025-07-07 20:34:42 +0530

    code added again

--- DiceRoller/ContentView.swift
+++ DiceRoller/ContentView.swift
@@ -1,21 +1,48 @@
-//
-//  ContentView.swift
-//  DiceRoller
-//
-//  Created by Yagnik Patel on 07/07/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State var leftDiceImage = 1
+    @State var rightDiceImage = 1
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack{
+                Image("diceeLogo")
+                Spacer()
+                HStack(spacing:30){
+                    diceRoller(n: leftDiceImage)
+                    diceRoller(n: rightDiceImage)
+                }
+                Spacer()
+                Button {
+                    // write something
+                    self.leftDiceImage = Int.random(in: 1...6)
+                    self.rightDiceImage = Int.random(in: 1...6)
+                } label: {
+                    Text("Roll")
+                        .padding()
+                        .frame(width: 200, height: 60)
+                        .foregroundColor(.white)
+                        .font(.system(size: 30))
+                        .background(Color.red)
+                        .cornerRadius(20)
+                }
+                .padding(.bottom)
+                
+            }
         }
-        .padding()
+    }
+}
+
+struct diceRoller: View {
+    var n: Int
+    var body: some View{
+            Image("dice\(n)")
+                .resizable()
+                .frame(width: 150, height: 150)
     }
 }
 
