041fd5bef1ddb201aedad4f7d85860bc5bd6a59d
developer-yagnik <yagnikpatel5253@gmail.com>
2025-07-07 09:43:07 +0530

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Yagnik Patel on 07/07/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
